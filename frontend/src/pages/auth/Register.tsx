bf7f30008c5e932a361bd3121831404b9f10b2d5
saltylex <lexigoia@gmail.com>
2025-01-26 16:24:48 +0200

    uite leo butonul

--- frontend/src/pages/auth/Register.tsx
+++ frontend/src/pages/auth/Register.tsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import api from '../../client/api';
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faEye, faEyeSlash} from "@fortawesome/free-solid-svg-icons";
 
 const Register: React.FC = () => {
     const [formData, setFormData] = useState({
@@ -12,6 +14,10 @@ const Register: React.FC = () => {
         address: '',
     });
 
+    // Separate state for the visibility of each password field
+    const [passwordVisible, setPasswordVisible] = useState(false);
+    const [confirmedPasswordVisible, setConfirmedPasswordVisible] = useState(false);
+
     const handleRegister = async () => {
         try {
             await api.post('/user/register', formData);
@@ -22,19 +28,66 @@ const Register: React.FC = () => {
         }
     };
 
+    const togglePasswordVisibility = () => {
+        setPasswordVisible(prev => !prev);
+    };
+
+    const toggleConfirmedPasswordVisibility = () => {
+        setConfirmedPasswordVisible(prev => !prev);
+    };
+
     return (
-        <div className="mt-20 flex flex-col items-center justify-center h-full" >
+        <div className="mt-20 flex flex-col items-center justify-center h-full">
             <form className="grid grid-cols-2 items-center justify-center h-full gap-6" onSubmit={(e) => { e.preventDefault(); handleRegister(); }}>
                 {Object.keys(formData).map((key) => (
-                    <input className="p-4 bg-violet-300 text-black rounded-3xl"
-                        key={key}
-                        type="text"
-                        placeholder={key}
-                        value={(formData as any)[key]}
-                        onChange={(e) => setFormData({ ...formData, [key]: e.target.value })}
-                    />
+                    key === 'password' ? (
+                        <div key={key} className="relative">
+                            <input
+                                className="p-4 bg-violet-300 text-black rounded-3xl"
+                                type={passwordVisible ? 'text' : 'password'}
+                                placeholder={key}
+                                value={(formData as any)[key]}
+                                onChange={(e) => setFormData({ ...formData, [key]: e.target.value })}
+                            />
+                            <button
+                                type="button"
+                                className="absolute right-2 top-1/2 transform -translate-y-1/2 text-gray-500"
+                                onClick={togglePasswordVisibility}
+                            >
+                                {!passwordVisible ? <FontAwesomeIcon className="text-white" icon={faEyeSlash} /> : <FontAwesomeIcon className="text-white" icon={faEye} />}
+                            </button>
+                        </div>
+                    ) : key === 'confirmedPassword' ? (
+                        <div key={key} className="relative">
+                            <input
+                                className="p-4 bg-violet-300 text-black rounded-3xl"
+                                type={confirmedPasswordVisible ? 'text' : 'password'}
+                                placeholder={key}
+                                value={(formData as any)[key]}
+                                onChange={(e) => setFormData({ ...formData, [key]: e.target.value })}
+                            />
+                            <button
+                                type="button"
+                                className="p-0 absolute right-2 top-1/2 transform -translate-y-1/2 text-gray-500"
+                                onClick={toggleConfirmedPasswordVisibility}
+                            >
+                                {!confirmedPasswordVisible? <FontAwesomeIcon className="text-white" icon={faEyeSlash} /> : <FontAwesomeIcon className="text-white" icon={faEye} />}
+                            </button>
+                        </div>
+                    ) : (
+                        <input
+                            className="p-4 bg-violet-300 text-black rounded-3xl"
+                            key={key}
+                            type="text"
+                            placeholder={key}
+                            value={(formData as any)[key]}
+                            onChange={(e) => setFormData({ ...formData, [key]: e.target.value })}
+                        />
+                    )
                 ))}
-                <button  className="hover:bg-gradient-to-b from-violet-50 to-violet-300 hover:text-black" type="submit">Register</button>
+                <button className="hover:bg-gradient-to-b from-violet-50 to-violet-300 hover:text-black" type="submit">
+                    Register
+                </button>
             </form>
         </div>
     );
